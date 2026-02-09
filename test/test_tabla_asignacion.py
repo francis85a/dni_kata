e27791f46f1f6dfc209def1ce59fad94a925df04
ccastroto <castroaldrovercarlossangrinha@gmail.com>
2026-02-09 12:55:02 +0100

    test(tabla_Asignacion): Definir los casos test y que pase el primer caso test de letras

--- test/test_tabla_asignacion.py
+++ test/test_tabla_asignacion.py
@@ -1,48 +1,48 @@
-#from test.dni_correctos import CASOS_TEST_CORRECTOS
-#from test.dni_incorrectos import CASOS_TEST_LETRA_PROHIBIDA
-#import pytest
-#from src.tablaAsignacion import TablaAsignacion
-#
-#
-#@pytest.fixture(name="tabla")
-#def tablaAsignacion():
-#    return TablaAsignacion()
-#
-#
-#def test_getTabla(tabla):
-#
-#    assert tabla.getTabla() == [
-#        "T",
-#        "R",
-#        "W",
-#        "A",
-#        "G",
-#        "M",
-#        "Y",
-#        "F",
-#        "P",
-#        "D",
-#        "X",
-#        "B",
-#        "N",
-#        "J",
-#        "Z",
-#        "S",
-#        "Q",
-#        "V",
-#        "H",
-#        "L",
-#        "C",
-#        "K",
-#        "E",
-#    ]
-#
-#
-#def test_getLetra(tabla):
-#    assert tabla.getLetra(0) == "T"
-#    assert tabla.getLetra(22) == "E"
-#    assert tabla.getLetra(30) == "Posicion letra fuera de rango"
-#
+from test.dni_correctos import CASOS_TEST_CORRECTOS
+from test.dni_incorrectos import CASOS_TEST_LETRA_PROHIBIDA
+import pytest
+from src.tabla_asignacion import TablaAsignacion
+
+
+@pytest.fixture(name="tabla")
+def tablaAsignacion():
+    return TablaAsignacion()
+
+
+def test_getTabla(tabla):
+
+    assert tabla.getTabla() == [
+        "T",
+        "R",
+        "W",
+        "A",
+        "G",
+        "M",
+        "Y",
+        "F",
+        "P",
+        "D",
+        "X",
+        "B",
+        "N",
+        "J",
+        "Z",
+        "S",
+        "Q",
+        "V",
+        "H",
+        "L",
+        "C",
+        "K",
+        "E",
+    ]
+
+
+def test_getLetra(tabla):
+    assert tabla.getLetra(0) == "T"
+    assert tabla.getLetra(22) == "E"
+    assert tabla.getLetra(30) == "Posicion letra fuera de rango"
+
 #
 #def test_getModulo(tabla):
 #    assert tabla.getModulo() == 23
