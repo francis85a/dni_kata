47fa974e87922f3bf03c29d3d3274f852c6deb4d
ccastroto <castroaldrovercarlossangrinha@gmail.com>
2026-02-09 12:54:28 +0100

    feat(tabla_Asignacion): Definir la posicion de la lista de letras

--- src/tabla_asignacion.py
+++ src/tabla_asignacion.py
@@ -2,3 +2,12 @@ class TablaAsignacion:
     def __init__(self):
         self.tabla = ["T", "R", "W", "A", "G", "M", "Y", "F", "P", "D", "X", "B", "N", "J", "Z", "S", "Q", "V", "H", "L", "C", "K", "E",]
         
+
+    def getTabla(self):
+        return self.tabla
+
+    def getLetra(self, posicion):
+        try:
+            return self.tabla[posicion]
+        except IndexError:
+            return "Posicion letra fuera de rango"
